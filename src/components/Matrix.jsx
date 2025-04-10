0d01a49d783755f499228ab11e9aed194bd64d4a
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2025-04-10 22:43:53 +0800

    Finish matrixInputs overall layout

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,24 +1,25 @@
-export default function Matrix(){
-  return(
-    <div className="flex  gap-[10px]">
-    <LeftBracket rowSize={3}/>
-    <RightBracket rowSize={3} />
+export default function Matrix() {
+  return (
+    <div className="flex items-center gap-[10px]">
+      <LeftBracket rowSize={3} />
+      <MatrixInputs rowSize={3} colSize={4} />
+      <RightBracket rowSize={3} />
     </div>
   )
 }
 
-function LeftBracket({rowSize}){
+function LeftBracket({ rowSize }) {
   const rowHeight = {
-    1:"h-[90px]",
-    2:'h-[160px]',
-    3:'h-[230px]',
-    4:'h-[300px]'
+    1: "h-[90px]",
+    2: 'h-[160px]',
+    3: 'h-[230px]',
+    4: 'h-[300px]'
   }
-  
+
   const containerHeight = rowHeight[rowSize]
 
 
-  return(
+  return (
     <div className={`${containerHeight} w-[40px] relative `}>
       <div className="absolute left-0 h-full bg-matrixBracket w-[10px]"></div>
       <div className="absolute top-0 left-[10px] bg-matrixBracket h-[10px] w-[30px]"></div>
@@ -27,18 +28,18 @@ function LeftBracket({rowSize}){
   )
 }
 
-function RightBracket({rowSize}){
+function RightBracket({ rowSize }) {
   const rowHeight = {
-    1:"h-[90px]",
-    2:'h-[160px]',
-    3:'h-[230px]',
-    4:'h-[300px]'
+    1: "h-[90px]",
+    2: 'h-[160px]',
+    3: 'h-[230px]',
+    4: 'h-[300px]'
   }
-  
+
   const containerHeight = rowHeight[rowSize]
 
 
-  return(
+  return (
     <div className={`${containerHeight} w-[40px] relative `}>
       <div className="absolute right-0 h-full bg-matrixBracket w-[10px]"></div>
       <div className="absolute top-0 right-[10px] bg-matrixBracket h-[10px] w-[30px]"></div>
@@ -47,3 +48,22 @@ function RightBracket({rowSize}){
   )
 }
 
+function MatrixInputs({ rowSize, colSize }) {
+  const numberOfInputs = rowSize * colSize
+  const inputsArray = Array.from({ length: numberOfInputs }, () => '');
+
+  return (
+    <div className={`grid gap-[20px]`}
+      style={{
+        gridTemplateColumns: `repeat(${colSize},50px)`,
+      }}
+    >
+      {inputsArray.map((_, index) => {
+        return (
+          <input key={"matrix" + index} className="bg-matrixInputBackground 
+          w-[50px] aspect-square"></input>
+        )
+      })}
+    </div>
+  )
+}
