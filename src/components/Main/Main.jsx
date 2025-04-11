a6a74c5f04a57b2f16036f39e7a13c76206bbe9a
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2025-04-11 13:57:07 +0800

    Add Start Button validation

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import DimensionInput from "./DimensionInput"
 import Matrix from "./Matrix"
 import multiplicationSymbol from '@images/multiplicationSymbol.svg'
@@ -12,36 +12,52 @@ export default function Main() {
 
   return (
     <main className="flex flex-col items-center gap-[45px]">
-    <div className="flex justify-center items-center gap-[50px] mt-[40px]">
-      <div className="flex flex-col gap-[30px]">
-        <div className="w-[360px] h-[300px] flex flex-col justify-center items-center">
-          <Matrix row={matrix1Pos.row} column={matrix1Pos.col}></Matrix>
+      <div className="flex justify-center items-center gap-[50px] mt-[40px]">
+        <div className="flex flex-col gap-[30px]">
+          <div className="w-[360px] h-[300px] flex flex-col justify-center items-center">
+            <Matrix row={matrix1Pos.row} column={matrix1Pos.col}></Matrix>
+          </div>
+          <DimensionInput matrixPos={matrix1Pos} setMatrixPos={setMatrix1Pos} />
         </div>
-        <DimensionInput matrixPos={matrix1Pos} setMatrixPos={setMatrix1Pos} />
-      </div>
-      <img src={multiplicationSymbol} className="mb-[66px]" alt="" />
-      <div className="flex flex-col gap-[30px]">
-        <div className="w-[360px] h-[300px] flex flex-col justify-center items-center">
-          <Matrix row={matrix2Pos.row} column={matrix2Pos.col}></Matrix>
+        <img src={multiplicationSymbol} className="mb-[66px]" alt="" />
+        <div className="flex flex-col gap-[30px]">
+          <div className="w-[360px] h-[300px] flex flex-col justify-center items-center">
+            <Matrix row={matrix2Pos.row} column={matrix2Pos.col}></Matrix>
+          </div>
+          <DimensionInput matrixPos={matrix2Pos} setMatrixPos={setMatrix2Pos} />
         </div>
-        <DimensionInput matrixPos={matrix2Pos} setMatrixPos={setMatrix2Pos} />
       </div>
-    </div>
-    <StartButton />
+      <StartButton matrix1Pos={matrix1Pos} matrix2Pos={matrix2Pos} />
     </main>
   )
 }
 
-function StartButton(){
-  const setStarted = useStartedStore((state)=>state.setStarted)
+function StartButton({ matrix1Pos, matrix2Pos }) {
+  const setStarted = useStartedStore((state) => state.setStarted)
+  const [isEnabled, setIsEnabled] = useState(true)
+  useEffect(() => {
+    if (matrix1Pos.row === '' || matrix1Pos.col === ''
+      || matrix2Pos.row === '' || matrix2Pos.col === '') {
+      setIsEnabled(false)
+      return
+    }
+
+    if (matrix1Pos.row !== matrix2Pos.col) {
+      setIsEnabled(false)
+      return
+    }
+    setIsEnabled(true)
+  }, [matrix1Pos, matrix2Pos, setIsEnabled])
 
   return (
-    <button 
-    onClick={()=>{
-      setStarted(true)
-    }}
-    className="text-[64px] w-[250px] h-[74px] flex 
-    justify-center items-center tracking-[-0.05em] cursor-pointer
-    border border-white">START</button>
+    <button
+      onClick={() => {
+        setStarted(true)
+      }}
+      disabled={!isEnabled}
+      className={`text-[64px] w-[250px] h-[74px] flex 
+    justify-center items-center tracking-[-0.05em] 
+    ${isEnabled?'cursor-pointer':'opacity-50 cursor-default'}
+    border border-white`}>START</button>
   )
 }
